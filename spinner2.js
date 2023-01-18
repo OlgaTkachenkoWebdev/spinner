d42a2cc73cfab92082ea1efa969e03cdd6ad319c
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-17 22:53:35 -0800

    spinner2 added

---
+++ spinner2.js
@@ -0,0 +1,45 @@
+let spinner = '|/-\\|/-\\|'
+let delay = 300;
+
+for (let char of spinner + "\n") {
+  setTimeout(() => {
+    process.stdout.write('\r' + char);
+  }, delay += 200)
+}
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   '); 
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1700);
+
+// setTimeout(() => {
+//   console.log();
+// }, 1900);
